44c75e4dd3bdc610aaa3f8f846d16316a09d1a58
mticciati <matthewticciati@gmail.com>
2017-08-01 11:38:32 -0700

    add nodemon

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ passport.use(
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-  }, accessToken => {
+  }, (accessToken, refreshToken, profile, done) => {
     console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
   })
 );
 
